0600ccb8e2ad0f09028056ec2dc26a5c185a14a2
MichaelTP <michaeljtp1@gmail.com>
2020-01-27 10:04:54 +0000

    estructura de carpetas

---
+++ src/main/java/dad/swordrunner/Main.java
@@ -0,0 +1,14 @@
+package dad.swordrunner;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class Main extends GameApplication {
+
+	@Override
+	protected void initSettings(GameSettings settings) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
